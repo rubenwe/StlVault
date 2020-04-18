017073f126f9faab0e9362052db67ba5f0c70d0b
Ruben Weituschewitz <weituschewitz@gmail.com>
2020-04-18 16:02:21 +0200

    Display app version

--- Assets/Scripts/Config/ApplicationSettings.cs
+++ Assets/Scripts/Config/ApplicationSettings.cs
@@ -5,8 +5,8 @@ namespace StlVault.Config
     internal class ApplicationSettings
     {
         public ushort UiScalePercent { get; set; } = 100;
-        public LogLevel LogLevel { get; set; } = LogLevel.Info;
-        public ushort ImportParallelism { get; set; } = 2;
+        public LogLevel LogLevel { get; set; } = LogLevel.Trace;
+        public ushort ImportParallelism { get; set; } = 1;
         public ushort PreviewJpegQuality { get; set; } = 70;
         public ushort PreviewResolution { get; set; } = 10;
         public ushort ScrollSensitivity { get; set; } = 150;
