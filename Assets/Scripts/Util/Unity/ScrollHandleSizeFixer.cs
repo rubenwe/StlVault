0caca4ba6639463e918145e497b681fb6dd37122
Ruben Weituschewitz <weituschewitz@gmail.com>
2020-04-22 00:26:00 +0200

    Set minimal size for scroll handles

---
+++ Assets/Scripts/Util/Unity/ScrollHandleSizeFixer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace StlVault.Util.Unity
+{
+    [RequireComponent(typeof(Scrollbar))]
+    public class ScrollHandleSizeFixer : MonoBehaviour
+    {
+        [SerializeField] private float _minHandleSize = 0.1f;
+        private Scrollbar _scrollBar;
+
+        private void Awake()
+        {
+            _scrollBar = GetComponent<Scrollbar>();
+        }
+
+        private void LateUpdate()
+        {
+            _scrollBar.size = Mathf.Max(_scrollBar.size, _minHandleSize);
+        }
+    }
+}
